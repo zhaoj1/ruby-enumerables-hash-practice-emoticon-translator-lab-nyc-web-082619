68de400b6bcdf74930bf90778b25b0d94a3cf8c9
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-09 22:58:50 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,30 @@
-# require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(path)
+  library = { "get_meaning" => {}, "get_emoticon" => {} }
+  YAML.load_file(path).each do |meaning, (english, japanese)|
+    library["get_emoticon"][english] = japanese
+    library["get_meaning"][japanese] = meaning
+  end
+  library
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(path, eng_emoticon)
+    library = load_library(path)
+    result = library["get_emoticon"][eng_emoticon]
+    if result
+      result
+    else
+      "Sorry, that emoticon was not found"
+    end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(path, japanese_emoticon)
+    library = load_library(path)
+    result = library["get_meaning"][japanese_emoticon]
+    if result
+      result
+    else
+      "Sorry, that emoticon was not found"
+    end
 end
